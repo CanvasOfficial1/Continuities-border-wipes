0bf606b8b8261ed39897c373ed4231a2732ef9ba
Moon Moth Inc <34354887+CanvasOfficial1@users.noreply.github.com>
2018-01-26 13:41:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,38 @@
 		var content = element.innerHTML
 		, borderCont = document.createElement('div')
 		, border = document.createElement('div')
-		, elWidth = element.offsetWidth
+		, elWidth = element.offsetWidt('diag')
 		, elHeight = element.offsetHeight
 		, diag = Math.ceil(Math.sqrt((elWidth * elWidth) + (elHeight * elHeight)))
+		, borderEnd = document.createElement('end')
+                , borderLength = document.createElement ('diag')
+		, borderColor = rngColors ('end')
+		, borderAdd = document.createElement ('diag')
+		, borderAdd = searchClick(borderColor(borderAdd))
+		, borderColorHex = document.CreateElement ('diag')
 		;
+		
 
-
+		{window.addEvent('diag',function(){
+			Array.prototype.forEach.call(document.borderColorHex('rngColors'), _initElement);
+		element.insertBefore(borderColorHex, element.firstChild)};
+		
+		
 		borderCont.className = 'border-container';
-		borderCont.style.width = diag + 'px'; borderCont.style.height = diag + 'px';
+		borderCont.style.width = diag + 'px'; borderCont.style.height = diag + 'px';hex
 		borderCont.style.top = (elHeight - diag) / 2 + 'px';
 		borderCont.style.left = (elWidth - diag) / 2 + 'px';
-		borderCont.style.transform = 'rotate(45deg) translate3d(-' + diag + 'px, 0, 0)';
+		borderCont.style.transform = 'rotate(90deg) translate3d(-' + diag + 'px, 0, 0)';
 		border.className = 'border';
 		border.style.width = elWidth + 'px'; border.style.height = elHeight + 'px';
 		border.style.top = (diag - elHeight) / 2 + 'px';
 		border.style.left = (diag - elWidth) / 2 + 'px';
 		border.style.transform = 'translate3d(' + diag + 'px, 0, 0) rotate(-45deg)';
-
+                border.style.bottom = (diag - elLength)
 		borderCont.appendChild(border);
 		element.insertBefore(borderCont, element.firstChild);
-
+		element.insertBorder(rngColors(searchClick(borderAdd(borderColorHex * borderColorHex))))
+		
 		borderCont.style.overflow = 'visible';
 		setTimeout(function() {
 			borderCont.style.overflow = 'hidden';
