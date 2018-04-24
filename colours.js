15a799de296e9036367f3fafa2f22d78955e8136
Moon Moth Inc <34354887+CanvasOfficial1@users.noreply.github.com>
2018-04-24 13:27:20 -0400

    Update colours.js

--- colours.js
+++ colours.js
@@ -1,1 +1,11 @@
+var. createRNG = create.elementDoc
+    Array.createPrototype('colorGen')
+
+{//TEST
+  
+      colorGen = createDocument.createElement {var = hex 'template"}    
+                                               
+                                              }
+        
+
 
